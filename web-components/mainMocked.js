690d81270d267e312850f6d03b79d00b561b44da
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-07-04 21:35:39 +0200

    Add reset css properties

--- web-components/mainMocked.js
+++ web-components/mainMocked.js
@@ -3,6 +3,7 @@ import jsonData from "./src/components/topbar/tests/sampleData.json";
 import "./src/components/topbar/topbar";
 import "./src/common/global-colors.css";
 import "./src/common/global-properties.css";
+import "./src/common/global-reset.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const topBar = document.querySelector("dynamic-topbar");
